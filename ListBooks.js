a543ff02bcad4c7163b42d8645b022f249ec333c
geetads <45489768+geetads@users.noreply.github.com>
2019-12-21 18:40:57 -0500

    Update ListBooks.js

--- ListBooks.js
+++ ListBooks.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import SearchBook from './SearchBook'
 
-const ListBooks =(props)=>{
+const ListBooks =(props) =>{
+
 
 const {books,onUpdateShelf}=props;
 
@@ -14,85 +17,39 @@ if (onUpdateShelf){
 
 }
 
-const currentlyReadingBooks=books.length > 0 && books.filter(book=>book.shelf==='currentlyReading');
-const wantToReadBooks=books.length>0 && books.filter(book=>book.shelf==='wantToRead');
-const read=books.length>0 && books.filter(book=>book.shelf==='read');
-
-
-
-return (
-
-<div className="list-books-content">
-    <div className="bookshelf">
-      <h2 className= 'bookshelf-title'> Currently Reading </h2>
-      <div className='bookshelf-books'></div>
-        <ol className="books-grid">{currentlyReadingBooks.length>0 && (currentlyReadingBooks.map((book,i)=>(
-            <li key={i}>
-                       <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
-                                <option name="wantToRead" value="wantToRead">Want to Read</option>
-                                <option name="read" value="read">Read</option>
-                                <option name="none" value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          {Array.isArray(book.authors)?
-                          book.authors.map((author,i)=>(
-                            <div key={i} className="book-authors">{author}</div>
-                          ))
-                        :
-                        false
-                        }
-</div>
-
-
-            </li>
-        )))
-                      }
-            </ol>
-    </div>
-
-    <h2 className= 'bookshelf-title'> Want To Read </h2>
-      <div className='bookshelf-books'></div>
-        <ol className="books-grid">{wantToReadBooks.length>0 && (wantToReadBooks.map((book,i)=>(
-            <li key={i}>
-                       <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
-                                <option name="wantToRead" value="wantToRead">Want to Read</option>
-                                <option name="read" value="read">Read</option>
-                                <option name="none" value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          {Array.isArray(book.authors)?
-                          book.authors.map((author,i)=>(
-                            <div key={i} className="book-authors">{author}</div>
-                          ))
-                        :
-                        false
-                        }
-                  </div>
-            </li>
-           )))
-          }
-          </ol>
+return (   
+  <div>
   
+        <ol className="books-grid">{books.length>0 && (books.map((book,i)=>(
+            <li key={i}>
+                       <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
+                            <div className="book-shelf-changer">
+                              <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
+                                <option value="none" disabled>Move to...</option>
+                                <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
+                                <option name="wantToRead" value="wantToRead">Want to Read</option>
+                                <option name="read" value="read">Read</option>
+                                <option name="none" value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          {Array.isArray(book.authors)?
+                          book.authors.map((author,i)=>(
+                            <div key={i} className="book-authors">{author}</div>
+                          ))
+                        :
+                        false
+                        }
+                       </div>
+                    </li>
+                        )))
+                      }
+                    </ol>
 
-<h2 className= 'bookshelf-title'> Read </h2>
-      <div className='bookshelf-books'></div>
-        <ol className="books-grid">{read.length > 0 && (read.map((book,i)=>(
+        <ol className="books-grid">{books.length>0 && (books.map((book,i)=>(
             <li key={i}>
                        <div className="book">
                           <div className="book-top">
@@ -120,11 +77,43 @@ return (
            )))
           }
           </ol>
-         
-</div>
 
-)        
+
+
+        <ol className="books-grid">{books.length > 0 && (books.map((book,i)=>(
+            <li key={i}>
+                       <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
+                            <div className="book-shelf-changer">
+                              <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
+                                <option value="none" disabled>Move to...</option>
+                                <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
+                                <option name="wantToRead" value="wantToRead">Want to Read</option>
+                                <option name="read" value="read">Read</option>
+                                <option name="none" value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          {Array.isArray(book.authors)?
+                          book.authors.map((author,i)=>(
+                            <div key={i} className="book-authors">{author}</div>
+                          ))
+                        :
+                        false
+                        }
+                  </div>
+            </li>
+           )))
+          }
+          </ol>
+          </div>
+         
+);
 }
+
+
 
 
         
@@ -132,3 +121,9 @@ return (
 
     
 export default ListBooks
+
+        
+
+
+    
+
