2f13b3627ec277aa8ac5d7d1afc78f1f481e69dc
geetads <45489768+geetads@users.noreply.github.com>
2019-12-29 15:29:43 -0500

    Update SearchBook.js

--- SearchBook.js
+++ SearchBook.js
@@ -9,56 +9,74 @@ class SearchBooks extends React.Component {
   state = {
     search:'',
     showSearchPage: false
+    
   }
 
- static propTypes = {
-    books: PropTypes.array.isRequired,
-    onUpdateShelf: PropTypes.func.isRequired,
-  }
-  handleChange=(e)=>{
+
+handleChange=(e)=>{
     console.log(e.target.value);
-    this.props.onSearchBook(e.target.value);
-  }
+    this.setState({search:e.target.value});
+    if(this.props.onSearchBook && this.state.search!==""){
+      this.props.onSearchBook(e.target.value);
+    }
+  } 
+
+  onhandleSelectChange=(book,e)=>{
+    e.preventDefault();
+    const value=e.target.value;
+    console.log(value);
+    if (this.props.onUpdateShelf){
+      this.props.onUpdateShelf(book,value);
+    }
+  
+    }
+  
+  
 
   render() {
 
    
     const  {searchBookResult}=this.props;
-    
+    const {books,onUpdateShelf}=this.props;
 
 
     return( 
       
-
+<div>
         <div className="search-books">
+        
             <div className="search-books-bar">
               <Link to='/' className='close-search'>
                 close
               </Link>
-            
+              
               <div className="search-books-input-wrapper">
+            
                 <form className='search-book-form'>
                 <input 
                 type="text" 
                 placeholder="Search by title or author"
                 value={this.state.search}
                 onChange={this.handleChange}
+                
                 />
                 </form>
+                </div>
                 
 </div>
 </div>
              
 
-
+<div>
               <div className='bookshelf-books'></div>
-        <ol className="books-grid">{searchBookResult.length>0 && (searchBookResult.map((book,i)=>(
+        <ol className="books-grid">{ searchBookResult && searchBookResult.length>0  && (searchBookResult.map((book,i)=>(
             <li key={i}>
                        <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
                             <div className="book-shelf-changer">
-                              <select value={book.shelf}>
+                              <select value={book.Shelf} 
+                                       onChange={(e)=>this.onhandleSelectChange(book,e)}> >
                                 <option value="none" disabled>Move to...</option>
                                 <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
                                 <option name="wantToRead" value="wantToRead">Want to Read</option>
@@ -81,13 +99,10 @@ class SearchBooks extends React.Component {
                         </ol>
 
                             </div>
-                            
+                            </div>
                       );
                        }
-                       }
-                        
-           
-                
+                       }     
 
             
     
