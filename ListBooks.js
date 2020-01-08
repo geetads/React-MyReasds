08f21025ae6166e2a9c1609829ee59d4be74b89a
geetads <45489768+geetads@users.noreply.github.com>
2020-01-08 00:19:14 -0500

    Update ListBooks.js

--- ListBooks.js
+++ ListBooks.js
@@ -52,9 +52,13 @@ return (
         <ol className="books-grid">{books.length>0 && (books.map((book,i)=>(
             <li key={i}>
                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
+                           {book.imageLinks &&(
+                          <div className="book-top"> {book.imageLinks && ( <div className="book-cover"
+                           style={{ width: 128, 
+                          height: 193, 
+                            backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                          )}
+                          <div className="book-shelf-changer">
                               <select value={book.shelf} onChange={(e)=>handleSelectChange(book,e)}>
                                 <option value="none" disabled>Move to...</option>
                                 <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
@@ -63,7 +67,7 @@ return (
                                 <option name="none" value="none">None</option>
                               </select>
                             </div>
-                          </div>
+                          </div>)}
                           <div className="book-title">{book.title}</div>
                           {Array.isArray(book.authors)?
                           book.authors.map((author,i)=>(
